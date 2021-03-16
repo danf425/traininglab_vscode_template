cda9803df6860c9867a21a7136bd0daa4a194ae3
Jim Quilty <jim.quilty@kandji.io>
2021-03-16 17:56:25 -0400

    Updated code-server install script for centos 7
    Signed-off-by: Jim Quilty <jim.quilty@kandji.io>

--- terraform/aws/data-sources/setup-code-server.sh
+++ terraform/aws/data-sources/setup-code-server.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-
 # Prep for Code-Server Install
 sudo yum update --assumeyes
-curl -sL https://rpm.nodesource.com/setup_13.x | sudo bash -
-sudo yum install nodejs --assumeyes
-sudo yum groupinstall 'Development Tools' --assumeyes
-sudo yum install python2 libsecret-devel libX11-devel libxkbfile-devel --assumeyes
-npm config set python python2
+# curl -sL https://rpm.nodesource.com/setup_13.x | sudo bash -
+# sudo yum install nodejs --assumeyes
+# sudo yum groupinstall 'Development Tools' --assumeyes
+# sudo yum install python2 libsecret-devel libX11-devel libxkbfile-devel --assumeyes
+# npm config set python python2
 
 
 # Add repo and update git to v2
-sudo yum install -y https://repo.ius.io/ius-release-el7.rpm
-sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install -y yum-plugin-replace
-sudo yum replace -y git --replace-with git224
+# sudo yum install -y https://repo.ius.io/ius-release-el7.rpm
+# sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# sudo yum install -y yum-plugin-replace
+# sudo yum replace -y git --replace-with git224
 
 
 
 # Download and install Code Server
-sudo npm install -g --unsafe-perm code-server
+# sudo npm install -g --unsafe-perm code-server
+curl -fsSL https://code-server.dev/install.sh -o /tmp/install.sh
+chmod +x /tmp/install.sh
+/tmp/install.sh
 
 
 
@@ -42,7 +44,7 @@ sudo systemctl start code-server
 sudo systemctl enable code-server
 
 
-# rebuild rpm and yum database
-sudo rpm --rebuilddb
-sudo yum clean all
+# # rebuild rpm and yum database
+# sudo rpm --rebuilddb
+# sudo yum clean all
 
