73a7f802574160f4ee2fd66214456bb34e17fe06
Dan Flores Montanez <danf425@gmail.com>
2021-03-17 08:51:19 -0400

    added someuser... modified some other stuff

--- terraform/aws/data-sources/cloud-provision.sh
+++ terraform/aws/data-sources/cloud-provision.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Enable Password SSH
-echo "i made it this far"
+# # Enable Password SSH
+# echo "i made it this far"
 
-sudo sed -i "s/^\(ssh_pwauth:[[:space:]]*\)0/\11/" /etc/cloud/cloud.cfg
+# sudo sed -i "s/^\(ssh_pwauth:[[:space:]]*\)0/\11/" /etc/cloud/cloud.cfg
